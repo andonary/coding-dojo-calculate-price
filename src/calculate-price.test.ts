d96cc4c8cb4dbfbe5fcddff5e613d18e7c6fb91f
Ando Nary <ando.nary@gmail.com>
2021-12-06 13:35:17 +0100

    refacto

--- src/calculate-price.test.ts
+++ src/calculate-price.test.ts
@@ -5,30 +5,34 @@ function calculatePrice(articlePrice: string, quantity: number) {
     return value * quantity + "€";
 }
 
+interface InputCalculate {
+    articlePrice: string,
+    quantity: number
+}
+
 describe('Kata', () => {
+    const articlePrice = "1.21€";
+    function actPriceAndExpect(options: InputCalculate, expectedResult) {
+        // Act
+        const result = calculatePrice(options.articlePrice, options.quantity);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    }
+
     test('no reduction 3*1.21 equals 3.63€', async () => {
         // Arrange
-        const articlePrice = "1.21€";
         const quantity = 3;
         const noreductionPrice = "3.63€";
 
-        // Act
-        const result = calculatePrice(articlePrice, quantity);
-
-        // Assert
-        expect(result).toEqual(noreductionPrice);
+        actPriceAndExpect({articlePrice, quantity}, noreductionPrice);
     });
 
     test('no reduction 1*1.21 equals 1.21€', async () => {
         // Arrange
-        const articlePrice = "1.21€";
         const quantity = 1;
         const noreductionPrice = "1.21€";
 
-        // Act
-        const result = calculatePrice(articlePrice, quantity);
-
-        // Assert
-        expect(result).toEqual(noreductionPrice);
+        actPriceAndExpect({articlePrice, quantity}, noreductionPrice);
     });
 });
